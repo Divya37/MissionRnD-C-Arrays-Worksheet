c6cf06261659916fbbbbb15601842c5a1738a8e2
Divya37 <undidivya1996@gmail.com>
2015-11-22 12:03:17 +0500

    Submit Commit

--- src/removeArrayDuplicates.cpp
+++ src/removeArrayDuplicates.cpp
@@ -1,19 +1,27 @@
-/*
-OVERVIEW:  given an array that has duplicate values remove all duplicate values.
-E.g.: input: [1, 1, 2, 2, 3, 3], output [1, 2, 3]
-
-INPUTS: Integer array and length of the array.
-
-OUTPUT: Update input array by removing duplicate values.
-
-ERROR CASES: Return NULL for invalid inputs.
-
-NOTES: Don't create new array, try to change the input array.
-*/
-
 #include <stdio.h>
 
 void * removeArrayDuplicates(int *Arr, int len)
 {
+	int i, j,k;
+	if (Arr == NULL || len < 0)
+		return NULL;
+	else if (len == 1)
+		return Arr;
+	else
+	{
+		for (i = 0; i < len; i++)
+		{
+			for (j = i+1 ; j < len; j++)
+			{
+				if (Arr[i] == Arr[j])
+				{
+					for (k = j; k < len - 1; k++)
+						Arr[k] = Arr[k + 1];
+					len--;
+					
+				}
+			}
+		}		
+	}
 	return NULL;
 }
--- src/topKStudents.cpp
+++ src/topKStudents.cpp
@@ -1,26 +1,45 @@
-/*
-OVERVIEW:  You are given scores for students of a class in alphabetical order. Write a function that returns the names and scores of top K students.
-Input is array of structures of type student (each student has name and score).
-E.g.: If input is [ { "stud1", 20 }, { "stud2", 30 }, { "stud3", 40 } ] (three students with marks 20, 30 , 40) and K value is 2.
-return top 2 students [ { "stud2", 30 }, { "stud3", 40 } ]
-
-INPUTS: array of structures of type student, length of the array, integer K.
-
-OUTPUT: Return top K students from array. Output need not be in decreasing order of scores.
-
-ERROR CASES: Return NULL for invalid inputs.
-
-NOTES:
-*/
-
-#include <iostream>
 #include <malloc.h>
+#include<stdio.h>
 
-struct student {
+struct student
+{
 	char *name;
 	int score;
 };
+struct student ** topKStudents(struct student *students, int len, int K) 
+{
+	int i, j, score_temp;
+	char *name_temp;
+	struct student **std;	
+	name_temp = (char *)malloc(sizeof(char) * 5);	
+	if (K > len)
+		K = len;
+	std = (struct student **)malloc(sizeof(struct student*)*K);
 
-struct student ** topKStudents(struct student *students, int len, int K) {
-	return NULL;
+	if (students == NULL || len<0)
+			return NULL;
+	else
+	{
+		for (i = 0; i < len; i++)
+		{
+			for (j = 0; j < len; j++)
+			{
+				if (students[i].score <= students[j].score)
+				{
+					score_temp = students[i].score;
+					students[i].score = students[j].score;
+					students[j].score = score_temp;
+
+					name_temp = students[i].name;
+					students[i].name = students[j].name;
+					students[j].name = name_temp;
+
+				}
+			}
+		}
+		for (i = 0; i < K; i++)
+			std[i]= &students[i];			
+		return std;
+	}
+
 }
